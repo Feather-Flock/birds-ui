815176a148babe433c8901a09955e1e86841da8a
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-28 18:35:41 -0400

    Add ApolloProvider and client to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import './App.css';
 
-function App() {
+const client = new ApolloClient({
+  uri: "endpoint goes here",
+  cache: new InMemoryCache()
+});
+
+const App = () =>  {
   return (
-    <div className="App">
-      <h1 className="App-header"> Hello world!</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1 className="App-header"> Hello world!</h1>
+      </div>
+    </ApolloProvider>
   );
 }
 
