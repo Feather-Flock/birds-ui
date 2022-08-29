e97131f79aa4624a9caf90824be0bfbe1c14cf65
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-08-29 14:07:48 -0700

    adjust event card styling

--- src/Components/EventCard/EventCard.js
+++ src/Components/EventCard/EventCard.js
@@ -13,9 +13,15 @@ const EventCard = ({id, title, description, date, time, type}) => {
   } else if (type === "list") {
     return (
       <div className="event-list-item">
-        <h2>{title}</h2>
-        <p>{date}</p>
-        <p>{time}</p>
+        <div className="date-container">
+          <p className="event-date">date: {date}</p>
+        </div>
+        <div className="time-container">
+          <p className="event-time">{time}</p>
+        </div>
+        <div className="title-container">
+          <h2 className="event-title">{title}</h2>
+        </div>
       </div>
     )
   }
