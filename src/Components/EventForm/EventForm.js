a95110f41c0ba8c58e90e90a891b9504b6ecbb03
Blue Jessen <canealisblue@gmail.com>
2022-08-28 22:30:19 -0700

    Add onchange state change

--- src/Components/EventForm/EventForm.js
+++ src/Components/EventForm/EventForm.js
@@ -5,19 +5,25 @@ export default function EventForm() {
   const [eventDetails, setEventDetails] = useState({title: '',
   date:'',
   location:{},
-  description:'')
+  description:''})
+
+  const handleChange = (e) => {
+    const {name, value} = e.target;
+    setEventDetails({...eventDetails, [name]: value})
+  }
+
   return (
     <div className='form-wrapper'>
       <form>
         <h1 style={{textAlign: 'right',
           marginRight: '20%'}}>Create A New Event</h1>
-        <input className='event-input' type='text' placeholder='Add Title' name='title' value={eventDetails.title}/>
+        <input className='event-input' onChange={handleChange} type='text' placeholder='Add Title' name='title' value={eventDetails.title}/>
         <br/>
-        <input className='event-input' type='datetime-local' name='date' value={eventDetails.date}/>
+        <input className='event-input' onChange={handleChange} type='datetime-local' name='date' value={eventDetails.date}/>
         <br/>
-        <input className='event-input' type='text' name='location' value={eventDetails.location} placeholder='location'/>
+        <input className='event-input' onChange={handleChange} type='text' name='location' value={eventDetails.location} placeholder='location'/>
         <br/>
-        <input className='event-input description' type='text' name='description' value={eventDetails.description} placeholder='Add a description for your event'/>
+        <input className='event-input description' onChange={handleChange} type='text' name='description' value={eventDetails.description} placeholder='Add a description for your event'/>
         <button className='submit-event'>Add Event</button>
       </form>
     </div>
