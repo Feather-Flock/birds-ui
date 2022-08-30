aa73b1233bae9fefb1b8ae176b0c30477fa62c6c
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-08-29 22:40:31 -0700

    Map displaying on dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_USER_BY_ID } from "../../queries";
@@ -10,15 +10,38 @@ const Dashboard = () => {
     variables: {"id": "1"}
   })
 
+  useEffect(() => { //the following useEffect is what adds the map to the modal
+    if(!loading && !error) {
+      window.L.mapquest.key = process.env.REACT_APP_MAPQUEST_KEY;
+      const container = window.L.DomUtil.get("map")
+      if(container != null) {
+        container._leaflet_id = null;
+      }
+      var map = window.L.mapquest.map('map', { // 'map' is the id on line 57
+        center: [39.73352, -104.965847],
+        layers: window.L.mapquest.tileLayer('map'),
+        zoom: 12
+      });
+
+      window.L.marker([39.73352, -104.965847], { //to hover over marker it shows LV
+        icon: window.L.mapquest.icons.marker(),
+        draggable: false
+      }).bindPopup("Denver, CO").addTo(map);
+
+      window.L.circle([39.73352, -104.965847], { radius: 1000 }).addTo(map);
+    }
+  })
+
   if(loading) return "Loading..."
   if(error) return `Error! ${error.message}`
 
+  
   return (
     <div className="dashboard-container">
       <nav className="nav-bar">
         <h1 className="welcome-user">Welcome {data.userName}!</h1>
         <Link to="/new-event">
-          <button class="button-85" role="button">Create Event</button>
+          <button className="button-85" role="button">Create Event</button>
         </Link>
       
       </nav>
@@ -28,13 +51,13 @@ const Dashboard = () => {
           <Events events={data.rsvpEvents} type={"card"} />
         </div>
 
-        <div className="map-container">
+        <div id="map" className="map-container">
 
         </div>
 
         <div className="list-view-btn">
           <Link to="/Dashboard-List">
-            <button class="button-86" role="button">List View</button>
+            <button className="button-86" role="button">List View</button>
           </Link>
         </div>
       </div>
