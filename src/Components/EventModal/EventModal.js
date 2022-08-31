44966d45d09e1df8bfa0917d3ef49a5523c77282
Blue Jessen <canealisblue@gmail.com>
2022-08-30 17:17:37 -0700

    Insert skeletons for future functionality

--- src/Components/EventModal/EventModal.js
+++ src/Components/EventModal/EventModal.js
@@ -2,6 +2,7 @@ import React, {useState } from 'react'
 import './EventModal.css'
 import Modal from 'react-modal';
 import ReactDOM from 'react-dom';
+import { useNavigate } from 'react-router-dom';
 
 Modal.setAppElement('#root');
 
@@ -11,12 +12,15 @@ function EventModal({eventInfo}) {
 
   const openModal = () => {
     setIsOpen(true)
-    console.log('clicked')
   }
 
   const closeModal = () => {
     setIsOpen(false)
-    console.log('closed')
+  }
+
+  const getDirections = () => {
+    console.log('Insert url link here')
+    //insert navigationlink to directions 
   }
 
   const customStyles = {
@@ -32,11 +36,11 @@ function EventModal({eventInfo}) {
   familyID: 3,
   date: 'September 10,',
   time: '2:00pm-4:00pm',
-  location: 'The Art Museum',
+  address: 'The Art Museum',
   title:'A Trip To the Art Museum',
   description: 'We want to take our daughter to the new Frida Kahlo exhibit and would love if you would join us!',
   familyTags: ['mlm', '1 kid', 'daughter', 'gay', 'tween']}
-
+//remove above once bringing in data from destructured info
   return (
     <>
     <button onClick={openModal}>Open Modal</button>
@@ -52,7 +56,7 @@ function EventModal({eventInfo}) {
           <h1>{event.title}</h1>
           <h3>{`${event.date}  ${event.time}`}</h3>
           <img className='event-img' src='https://www.illustrationsof.com/royalty-free-rf-art-museum-clipart-illustration-by-nl-shop-stock-sample-432030.jpg'></img>
-          <p>Location: {event.location}</p>
+          <p>Location: {event.address}</p>
           <p className='event-description'>{event.description}</p>
         </div>
         <div>
@@ -61,7 +65,9 @@ function EventModal({eventInfo}) {
           <br/>
           <button> RSVP!</button>
           <br/>
-          <button>View Family Profile</button>
+          <button onClick={useNavigate('/familyProfile/3')}>View Family Profile</button>
+          //Add a dynamic link to family here
+          <button onClick={getDirections}>Get Directions</button>
         </div>
       </div>
     </Modal>
