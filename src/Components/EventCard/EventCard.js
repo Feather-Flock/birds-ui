4d27f3cd723cbc1b6eaccc05fe3f22a2d4888fe1
Amber Bodnar <amber.bodnar@gmail.com>
2022-08-31 20:09:00 -0400

    Add styling to EventCard attending container

--- src/Components/EventCard/EventCard.js
+++ src/Components/EventCard/EventCard.js
@@ -4,16 +4,29 @@ import "./EventCard.css"
 const EventCard = ({ id, title, description, date, time, type, handleClick }) => {
   if(type === "card") {
     return (
-      <div onClick={handleClick} id={id} className="event-card">
-        <h2 id={id}>{title}</h2>
-        <p>{date}</p>
-        <p>{time}</p>
+      <div className="event-container">
+        <h2 className="attending-header">Attending</h2>
+
+        <div className="event-picture-wrapper">
+          <img className="event-picture" alt="event festivities"></img>
+        </div>
+
+        <div className="event-info-container" >
+          <div className="event-details">
+            <p className="title">What: {title}</p>
+            <p className="date">When: {date}</p>
+            <p className="time">Time: {time}</p>
+          </div>
+          <p className="description">{description}</p>
+        </div>
+
+          <button className="view-details-button" onClick={handleClick}>View Details</button>
       </div>
     )
   } else if (type === "list") {
     return (
       <div className="event-container">
-        <h2 className="attending-header">Upcoming</h2>
+        <h2 className="upcoming-header">Upcoming in Your Area</h2>
 
         <div className="event-picture-wrapper">
           <img className="event-picture" alt="event festivities"></img>
