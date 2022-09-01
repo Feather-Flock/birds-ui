96c49199772cc7780a1caf3c891ce60b3a0a2406
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-08-31 22:15:04 -0700

    Break up nav bar with back button

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,36 +1,46 @@
 import React from 'react'
 import { Link, Route } from "react-router-dom";
 import "./Nav.css"
+import { BsArrowLeft } from 'react-icons/bs'
+import { IconContext } from "react-icons";
 
 const Nav = () => {
   return (
     <nav className="nav-bar">
-      <Link to="/profile">
-      <button className="radiant-button" role="button">View Profile</button>
-      </Link>
-      <Link to="/new-event">
-        <button className="radiant-button" role="button">Create Event</button>
-      </Link>
-      <Route exact path="/new-event">
-        <Link className="view-link" to="/">
-          <button className="radiant-button" role="button">Dashboard</button>
+      <div className="back-nav">
+        <Route exact path="/new-event">
+          <Link className="view-link" to="/">
+            <IconContext.Provider value={{ color: 'white', size: '20px' }}>
+              <button className="radiant-button" role="button"><BsArrowLeft /></button>
+            </IconContext.Provider>
+          </Link>
+        </Route>
+        <Route exact path="/profile">
+          <Link className="view-link" to="/">
+            <IconContext.Provider value={{ color: 'white', size: '20px' }}>
+              <button className="radiant-button" role="button"><BsArrowLeft /></button>
+            </IconContext.Provider>
+          </Link>
+        </Route>
+      </div>
+      <div className="site-nav">
+        <Link to="/profile">
+        <button className="radiant-button" role="button">View Profile</button>
         </Link>
-      </Route>
-      <Route exact path="/profile">
-        <Link className="view-link" to="/">
-          <button className="radiant-button" role="button">Dashboard</button>
+        <Link to="/new-event">
+          <button className="radiant-button" role="button">Create Event</button>
         </Link>
-      </Route>
-      <Route exact path="/Dashboard-List">
-        <Link className="view-link" to="/">
-          <button className="radiant-button" role="button">Map View</button>
-        </Link>
-      </Route>
-      <Route exact path="/">
-        <Link to="/Dashboard-List">
-            <button className="radiant-button" role="button">List View</button>
-        </Link>
-      </Route>
+        <Route exact path="/Dashboard-List">
+          <Link className="view-link" to="/">
+            <button className="radiant-button" role="button">Map View</button>
+          </Link>
+        </Route>
+        <Route exact path="/">
+          <Link to="/Dashboard-List">
+              <button className="radiant-button" role="button">List View</button>
+          </Link>
+        </Route>
+      </div>
     </nav>
   )
 }
