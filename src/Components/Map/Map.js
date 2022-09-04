fc4389721fdd3177d6b3c5ab3b19a76cb74500f8
Blue Jessen <canealisblue@gmail.com>
2022-09-03 17:39:26 -0700

    Initial map set up

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Map.css'
 
-export default Map() {
+export default Map({center, marker, markerLabel }) {
+
+  var map = window.L.mapquest.map('map', {
+    center: [location.place.geometry.coordinates[1],location.place.geometry.coordinates[0]],
+    layers: window.L.mapquest.tileLayer('map'),
+    zoom: 15
+  });
+
+  useEffect(()=> {
+  marker && markerLabel:window.L.marker( [location.place.geometry.coordinates[1],location.place.geometry.coordinates[0]], { //to hover over marker it shows event title
+    icon: window.L.mapquest.icons.flag({
+      primaryColor: '#000000',
+      secondaryColor: '#000000',
+      size: 'sm',
+      symbol: 'hello'
+    }),
+    draggable: true
+  }).bindPopup(location.name).addTo(map):return
+
+},[])
+
 return (
   <div id="map" className="event-form-map-container">
     </div>
