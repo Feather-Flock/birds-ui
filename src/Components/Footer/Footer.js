91ecd47a33094c9fc62d7b5ee5778d56b730d93b
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-09-05 12:05:19 -0700

    Organized footer in tables

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,47 +4,70 @@ import "./Footer.css";
 const Footer = () => {
 	return(
 		<footer className="footer-container">
-			<div className="right-footer">
-				<h4 className="footer-header">FrontEnd Team:</h4>
-        <div className="member-links">
-				  <p className="group-names">Blue Nealis:</p>
-          <a className="creds" href="https://github.com/">GitHub </a> 
-          <a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a>
-        </div>
-        <div className="member-links">
-          <p className="group-names">Amber Bodnar:</p>
-          <a className="creds" href="https://github.com/">GitHub </a> 
-          <a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a>
-        </div> 
-        <div className="member-links">
-          <p className="group-names">Trish Fox-Collis:</p>
-          <a className="creds" href="https://github.com/tfoxcollis">GitHub </a> 
-          <a className="creds" href="https://www.linkedin.com/in/trish-fox-collis/">LinkedIn </a>
-        </div>
-			</div>
-      <div className="left-footer">
-				<h4 className="footer-header">BackEnd Team:</h4>
-        <div className="member-links">
-				  <p className="group-names">Deannah Burke:</p>
-          <a className="creds" href="https://github.com/">GitHub </a> 
-          <a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a>
-        </div> 
-        <div className="member-links">
-          <p className="group-names">Clay Ash:</p>
-          <a className="creds" href="https://github.com/">GitHub </a> 
-          <a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a>
-        </div> 
-        <div className="member-links">
-          <p className="group-names">Ian Ross:</p>
-          <a className="creds" href="https://github.com/">GitHub </a> 
-          <a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a>
-        </div> 
-        <div className="member-links">
-          <p className="group-names">Luke Pascale:</p>
-          <a className="creds" href="https://github.com/">GitHub </a> 
-          <a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a>
-        </div> 
-			</div>
+      <table className="left-footer">
+        <tr>
+          <th>
+            <h4 className="footer-header">FrontEnd Team:</h4>
+          </th>
+        </tr>
+        <tr>
+          <div className="member-links">
+            <td><p className="group-names">Blue Nealis:</p></td>
+            <td><a className="creds" href="https://github.com/">GitHub </a> </td>
+            <td><a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a></td>
+          </div>
+        </tr>
+        <tr>
+          <div className="member-links">
+            <td><p className="group-names">Amber Bodnar:</p></td>
+            <td><a className="creds" href="https://github.com/">GitHub </a> </td>
+            <td><a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a></td>
+          </div> 
+        </tr>
+        <tr>
+          <div className="member-links">
+            <td><p className="group-names">Trish Fox-Collis:</p></td>
+            <td><a className="creds" href="https://github.com/tfoxcollis">GitHub </a> </td>
+            <td><a className="creds" href="https://www.linkedin.com/in/trish-fox-collis/">LinkedIn </a></td>
+          </div>
+        </tr>
+      </table>
+
+      <table className="right-footer">
+        <tr>
+          <th>
+				    <h4 className="footer-header">BackEnd Team:</h4>
+          </th>
+        </tr>
+        <tr>
+          <div className="member-links">
+            <td><p className="group-names">Deannah Burke:</p></td>
+            <td><a className="creds" href="https://github.com/">GitHub </a> </td>
+            <td><a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a></td>
+          </div> 
+        </tr>
+        <tr>
+          <div className="member-links">
+            <td><p className="group-names">Clay Ash:</p></td>
+            <td><a className="creds" href="https://github.com/">GitHub </a> </td>
+            <td><a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a></td>
+          </div> 
+        </tr>
+        <tr>
+          <div className="member-links">
+            <td><p className="group-names">Ian Ross:</p></td>
+            <td><a className="creds" href="https://github.com/">GitHub </a> </td>
+            <td><a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a></td>
+          </div>
+        </tr> 
+        <tr>
+          <div className="member-links">
+            <td><p className="group-names">Luke Pascale:</p></td>
+            <td><a className="creds" href="https://github.com/">GitHub </a> </td>
+            <td><a className="creds" href="https://www.linkedin.com/in/">LinkedIn </a></td>
+          </div> 
+        </tr>
+			</table>
 		</footer>
 	)
 }
