e803cc395051b789f32c2dca671bcee30a09beec
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-06 12:00:25 -0600

    Add basic structure for intercept

--- cypress/e2e/Dashboard.cy.js
+++ cypress/e2e/Dashboard.cy.js
@@ -1,23 +1,31 @@
-describe('empty spec', () => {
-  it('User sees a map with nearby events on page load', () => {
-    cy.visit('https://example.cypress.io')
+import { aliasQuery } from
+
+describe('Dashboard Tests', () => {
+  beforeEach(() => {
+    cy.intercept('POST', 'https://birds-api.herokuapp.com/graphql', (req) => {
+      aliasQuery(req, 'GetUserByID')
+    })
   })
-  it('User is welcomed onto the page', () => {
-    cy.visit('https://example.cypress.io')
-  })
-  it('User sees upcoming events they are attending', () => {
-    cy.visit('https://example.cypress.io')
-  })
-  it('User can click on an event their attending and have an event modal pop up with details', () => {
-    cy.visit('https://example.cypress.io')
-  })
-  it('User can click the markers for events and have a similar modal pop up', () => {
-    cy.visit('https://example.cypress.io')
-  })
-  it('User can navigate to an event form from their Dashboard', () => {
-    cy.visit('https://example.cypress.io')
-  })
-  it('User can navigate to their profile page', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+
+  // it('User sees a map with nearby events on page load', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
+  // it('User is welcomed onto the page', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
+  // it('User sees upcoming events they are attending', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
+  // it('User can click on an event their attending and have an event modal pop up with details', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
+  // it('User can click the markers for events and have a similar modal pop up', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
+  // it('User can navigate to an event form from their Dashboard', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
+  // it('User can navigate to their profile page', () => {
+  //   cy.visit('https://example.cypress.io')
+  // })
+});
