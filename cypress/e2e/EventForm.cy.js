1506fd66c75c7199657980b763f122e8c03399f5
Blue Jessen <canealisblue@gmail.com>
2022-09-05 18:45:35 -0700

    Add base skeleton for cypress event form and dashboard

---
+++ cypress/e2e/EventForm.cy.js
@@ -0,0 +1,32 @@
+describe('empty spec', () => {
+  it('User sees a map on page load', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('Can type into the form', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('When the user types into the location bar suggestions should populate', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('When a user selects a location from the suggestions the suggestion box should dissapear', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('When a user selects a location a map with a marker should populate', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('When a user tries to submit the form without all field filled out an error alert should popup', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('When a user submits the form a confirmation should pop up and the fields should reset', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('User should be able to navigate back to dashboard from event form', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('User should be able to navigate to user profile from event form', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('User should see new event on profile after they submitted event', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
