8762087be9ad10ed4d25191fc16fd5a93eb3e77f
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-06 14:46:37 -0600

    Add POST intercept and Event Container test

--- cypress/e2e/Dashboard.cy.js
+++ cypress/e2e/Dashboard.cy.js
@@ -1,30 +1,45 @@
-import { aliasQuery } from
+// import { aliasQuery } from
 
 describe('Dashboard Tests', () => {
   beforeEach(() => {
     cy.intercept('POST', 'https://birds-api.herokuapp.com/graphql', (req) => {
-      aliasQuery(req, 'GetUserByID')
+      if (req.body.operationName === 'user') {
+        req.alias = "GetUser"
+        req.continue()
+      }
     })
+    // need another intercept for the mapquest request
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('User sees upcoming events they are attending', () => {
+    cy.get('.rsvp-eventcards').should('be.visible')
+    .get('.event-container').contains('h2', 'Event you\'re Attending')
+    .get('.title').contains('p', 'What: ')
+    .get('.date').contains('p', 'When: ')
+    .get('.time').contains('p', 'Time: ')
   })
 
   // it('User sees a map with nearby events on page load', () => {
-  //   cy.visit('https://example.cypress.io')
+    // cy.get('#map').should('be.visible')
   // })
+
   // it('User is welcomed onto the page', () => {
   //   cy.visit('https://example.cypress.io')
   // })
-  // it('User sees upcoming events they are attending', () => {
-  //   cy.visit('https://example.cypress.io')
-  // })
+
   // it('User can click on an event their attending and have an event modal pop up with details', () => {
   //   cy.visit('https://example.cypress.io')
   // })
+
   // it('User can click the markers for events and have a similar modal pop up', () => {
   //   cy.visit('https://example.cypress.io')
   // })
+
   // it('User can navigate to an event form from their Dashboard', () => {
   //   cy.visit('https://example.cypress.io')
   // })
+
   // it('User can navigate to their profile page', () => {
   //   cy.visit('https://example.cypress.io')
   // })
