bb13ce5cf11f58d77f200299f54597db70b6cb0a
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-07 10:38:26 -0600

    Setup beforeEach for EventModal; pass first test

--- cypress/e2e/EventModal.cy.js
+++ cypress/e2e/EventModal.cy.js
@@ -1,5 +1,29 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { hasOperationName } from "../utils/graphql-test-utils";
+
+describe('Event Modal Tests', () => {
+  beforeEach(() => {
+    cy.intercept('POST', '/graphql', (req) => {
+      if (hasOperationName(req, 'event')) {
+        req.alias = 'queryEvent'
+        req.reply({
+          fixture: '../fixtures/event.json'
+        })
+      }
+
+      if (hasOperationName(req, 'user')) {
+        req.alias = 'queryUser'
+        req.reply({
+          fixture: '../fixtures/user.json'
+        })
+      }
+    })
+    cy.visit('http://localhost:3000/')
+    cy.get('.leaflet-marker-icon').should('be.visible').click()
+    cy.wait(`@queryEvent`)
+  });
+
+  it('User should see an event header', () => {
+    cy.get('.modal-header').contains('h1', 'Games at Fremont')
+  });
+
+});
