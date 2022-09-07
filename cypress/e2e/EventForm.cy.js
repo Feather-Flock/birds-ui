0e1238831bd6675b21d8b2807af89b01c358a654
Blue Jessen <canealisblue@gmail.com>
2022-09-07 13:00:01 -0700

    Add new event json file

--- cypress/e2e/EventForm.cy.js
+++ cypress/e2e/EventForm.cy.js
@@ -1,8 +1,21 @@
+import { hasOperationName } from "../utils/graphql-test-utils";
+
 describe('empty spec', () => {
   beforeEach(() => {
-    cy.intercept('/graphql',{
-      method: 'POST',
-      fixture: '../fixtures/user.json'
+    cy.intercept('POST', '/graphql', (req) => {
+      if (hasOperationName(req, 'event')) {
+        req.alias = 'queryEvent'
+        req.reply({
+          fixture: '../fixtures/event.json'
+        })
+      }
+
+      if (hasOperationName(req, 'user')) {
+        req.alias = 'queryUser'
+        req.reply({
+          fixture: '../fixtures/user.json'
+        })
+      }
     })
     cy.visit('http://www.birds-of-a-feather.net/new-event')
   });
@@ -38,14 +51,22 @@ describe('empty spec', () => {
   it.only('When a user submits the form a confirmation should pop up and the fields should reset', () => {
     cy.get('input').eq(0).click().type('Trip to Zoo')
     cy.get('input').eq(1).click()
-    cy.get('input').eq(2).click().type('The Zoo, Boulder')
+    cy.get('input').eq(2).click().type('The Zoo, Boulder CO')
     cy.get('option').eq(0).click()
     cy.get('input').eq(3).click().type('We want to go to the zoo with you!')
     cy.get('input').eq(0).click().should('have.value', 'Trip to Zoo')
     cy.get('input').eq(3).click().should('have.value','We want to go to the zoo with you!')
-    cy.intercept('')
-    cy.get('button').eq(0).click()
-  })
+    cy.get('button').eq(3).click()
+    cy.intercept('POST', '/graphql', (req) => {
+      if (hasOperationName(req, 'event')) {
+        req.alias = 'mutationEvent'
+        req.reply({
+          fixture: '../fixtures/event.json'
+        })
+      }
+    })
+    cy.get('alert')
+})
   it('User should be able to navigate back to dashboard from event form', () => {
     cy.visit('https://example.cypress.io')
   })
