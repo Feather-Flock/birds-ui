2fffcdd31adf43cf1187eb67092837211e3170dc
Blue Jessen <canealisblue@gmail.com>
2022-09-07 13:19:36 -0700

    Check nav to profile

--- cypress/e2e/EventForm.cy.js
+++ cypress/e2e/EventForm.cy.js
@@ -69,14 +69,13 @@ describe('empty spec', () => {
          expect(t).to.contains('New Event Made!');
       })
 })
-  it.only('User should be able to navigate back to dashboard from event form', () => {
+  it('User should be able to navigate back to dashboard from event form', () => {
     cy.get('button').eq(0).click()
     cy.url().should('eq', 'http://www.birds-of-a-feather.net/')
   })
   it('User should be able to navigate to user profile from event form', () => {
-    cy.visit('https://example.cypress.io')
+    cy.get('button').eq(1).click()
+    cy.url().should('eq', 'http://www.birds-of-a-feather.net/profile')
   })
-  it('User should see new event on profile after they submitted event', () => {
-    cy.visit('https://example.cypress.io')
-  })
+
 })
