404c62908381ecd950741e4e07784058978eeb53
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-06 23:52:55 -0600

    Pass username test

--- cypress/e2e/UserProfile.cy.js
+++ cypress/e2e/UserProfile.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('User Profile Test', () => {
+  beforeEach(() => {
+    cy.intercept('/graphql',{
+      method: 'POST',
+      fixture: '../fixtures/user.json'
+    })
+    cy.visit('http://localhost:3000/profile')
+  });
+
+  it('User should see their name at the top of the page', () => {
+    cy.get('.name-wrapper').contains('h2', 'BLUESKIIII')
+  });
+
+});
+
+
+// should have a family name
+// should have a location
+// should have a description
+// should have a section for events
