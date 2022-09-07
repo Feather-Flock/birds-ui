934291fc01d0a9165f3e3ff5363e770416f45765
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-07 10:27:42 -0600

    Pass tests for event modal from view details button/map marker; remove dead code

--- cypress/e2e/Dashboard.cy.js
+++ cypress/e2e/Dashboard.cy.js
@@ -1,12 +1,3 @@
-// describe('Dashboard Tests', () => {
-//   beforeEach(() => {
-//     cy.intercept('/graphql',{
-//       method: 'POST',
-//       fixture: '../fixtures/user.json'
-//     })
-//     cy.visit('http://localhost:3000/')
-//   });
-
 import { hasOperationName } from "../utils/graphql-test-utils";
 
 describe('Dashboard Tests', () => {
@@ -47,18 +38,18 @@ describe('Dashboard Tests', () => {
   });
 
   it('User can click on an event their attending and have an event modal pop up with details', () => {
-    // cy.get('.event-container').contains('button', 'View Details').click()
-    // .get('.event-modal').should('be.visible')
-
     cy.get('.view-details-button').first().contains('View Details').click()
     cy.wait(`@queryEvent`)
       .its('response.body.data.event')
       .should('have.property', "title")
   });
 
-  // it('User can click the markers for events and have a similar modal pop up', () => {
-  //     cy.visit('https://example.cypress.io')
-  //   })
+  it('User can click the markers for events and have a similar modal pop up', () => {
+    cy.get('.leaflet-marker-icon').should('be.visible').click()
+    cy.wait(`@queryEvent`)
+      .its('response.body.data.event')
+      .should('have.property', "title")
+  });
 
   it('User can navigate to an event form from their Dashboard', () => {
     cy.get('.site-nav').contains('button', 'Create Event').click()
@@ -71,5 +62,4 @@ describe('Dashboard Tests', () => {
     .url().should('contain', '/profile')
     .get('.name-wrapper').contains('h2', 'BLUESKIIII')
   });
-
 });
