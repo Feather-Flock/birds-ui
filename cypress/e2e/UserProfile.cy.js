f80350c12d6eee301cc6d0e81465060d31ee5efe
Amber Bodnar <amber.bodnar@gmail.com>
2022-09-06 23:59:12 -0600

    Pass test for user profile location

--- cypress/e2e/UserProfile.cy.js
+++ cypress/e2e/UserProfile.cy.js
@@ -7,14 +7,20 @@ describe('User Profile Test', () => {
     cy.visit('http://localhost:3000/profile')
   });
 
-  it('User should see their name at the top of the page', () => {
+  it('User should see their username at the top of the page', () => {
     cy.get('.name-wrapper').contains('h2', 'BLUESKIIII')
   });
+
+  it('User should see their location zipcode', () => {
+    cy.get('.material-symbols-outlined').should('be.visible')
+    .get('.location-wrapper').contains('h3', '80220')
+  });
+
+
 
 });
 
 
-// should have a family name
 // should have a location
 // should have a description
 // should have a section for events
